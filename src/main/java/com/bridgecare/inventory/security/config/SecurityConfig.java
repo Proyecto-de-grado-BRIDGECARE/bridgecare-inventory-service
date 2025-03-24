f970ffcbfd70adfdc5a79e8d8b2dcf329def0364
Daniel Vargas <dvargasp@javeriana.edu.co>
2025-03-24 01:38:45 -0500

    Update SecurityConfig to not use default config

--- src/main/java/com/bridgecare/inventory/security/config/SecurityConfig.java
+++ src/main/java/com/bridgecare/inventory/security/config/SecurityConfig.java
@@ -3,6 +3,8 @@ package com.bridgecare.inventory.security.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -28,5 +30,10 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
     }
 }
