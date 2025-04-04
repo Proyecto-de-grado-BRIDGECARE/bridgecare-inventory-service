fc440072ecee918bb0ea42a692ae35ac16de3acf
Daniel Vargas <dvargasp@javeriana.edu.co>
2025-04-03 23:06:22 -0500

    Add JsonProperty to properly fill the DTOs

--- src/main/java/com/bridgecare/inventory/models/dtos/InventarioDTO.java
+++ src/main/java/com/bridgecare/inventory/models/dtos/InventarioDTO.java
@@ -4,22 +4,39 @@ import java.util.List;
 
 import com.bridgecare.common.models.dtos.PuenteDTO;
 import com.bridgecare.common.models.dtos.UsuarioDTO;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.Data;
 
 @Data
 public class InventarioDTO {
+
     private String observaciones;
+
     private ApoyoDTO apoyo;
+
     private CargaDTO carga;
+
+    @JsonProperty("datos_administrativos")
     private DatosAdministrativosDTO datosAdministrativos;
+
+    @JsonProperty("datos_tecnicos")
     private DatosTecnicosDTO datosTecnicos;
+
+    @JsonProperty("miembros_interesados")
     private MiembrosInteresadosDTO miembrosInteresados;
+
     private List<PasoDTO> pasos;
+
+    @JsonProperty("posicion_geografica")
     private PosicionGeograficaDTO posicionGeografica;
+
     private SubestructuraDTO subestructura;
+
     private List<SuperestructuraDTO> superestructuras;
+
     private UsuarioDTO usuario;
+
     private PuenteDTO puente;
 
     public String getObservaciones() {
